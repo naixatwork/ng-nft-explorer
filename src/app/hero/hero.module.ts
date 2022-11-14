351126594bb41a5e9801fdee3984a281961b4169
naixatwork <naixatwork@gmail.com>
2022-11-14 12:37:17 +0330

    style(angular.json): set green purple pretheme

--- src/app/hero/hero.module.ts
+++ src/app/hero/hero.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { HeroRoutingModule } from './hero-routing.module';
-import { HeroComponent } from './hero.component';
+import {HeroRoutingModule} from './hero-routing.module';
+import {HeroComponent} from './hero.component';
 import {SharedModule} from "#shared/shared.module";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
   declarations: [
-    HeroComponent
+    HeroComponent,
   ],
-    imports: [
-        CommonModule,
-        HeroRoutingModule,
-        SharedModule
-    ]
+  imports: [
+    CommonModule,
+    HeroRoutingModule,
+    SharedModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ]
 })
 export class HeroModule { }
