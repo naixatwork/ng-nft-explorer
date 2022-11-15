60a05988bddc74d9359dcd334626a622658717b1
naixatwork <naixatwork@gmail.com>
2022-11-15 18:38:48 +0330

    feat(hero): added animations

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {animate, state, style, transition, trigger} from "@angular/animations";
 
-const fadeInOut = trigger("fadeInOut", [
+const controlHints = trigger("controlHints", [
   state("void", style({
     transform: 'translate(0%, -15%)',
     opacity: 0,
@@ -10,11 +10,39 @@ const fadeInOut = trigger("fadeInOut", [
   transition("void => *", [animate('250ms ease')]),
 ])
 
+const titleFadeIn = trigger("titleFadeIn", [
+  transition(
+    ":enter", [
+    style({
+      opacity: 0,
+    }),
+    animate("300ms ease-in",
+      style({
+        opacity: 1,
+      })
+    )
+  ])
+])
+
+const controlFadeIn = trigger("controlFadeIn", [
+  transition(
+    ":enter", [
+      style({
+        opacity: 0,
+      }),
+      animate("300ms 300ms ease-in",
+        style({
+          opacity: 1,
+        })
+      )
+    ])
+])
+
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss'],
-  animations: [fadeInOut]
+  animations: [controlHints, titleFadeIn, controlFadeIn]
 })
 export class HeroComponent implements OnInit {
   public form!: FormGroup;
